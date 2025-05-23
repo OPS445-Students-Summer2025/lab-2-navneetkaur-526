8e7b3dea71f28e4cbc55563333a789359840bbb1
Navneet kaur <nkaur526@myseneca.com>
2025-05-23 10:17:39 -0400

    lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Navneet kaur
+# Author ID: nkaur526
+# Date Created: 2025/05/23
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
